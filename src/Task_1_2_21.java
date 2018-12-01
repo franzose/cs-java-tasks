1b47ed622ef6cd7471f072d9e3176fc7d9563d75
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-01 19:33:56 +0700

    Solved task 1.2.21

---
+++ src/Task_1_2_21.java
@@ -0,0 +1,13 @@
+public class Task_1_2_21 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+
+        System.out.println(calc(Double.parseDouble(args[0])));
+    }
+
+    private static double calc(double t) {
+        return Math.sin(2 * t) + Math.sin(3 * t);
+    }
+}
