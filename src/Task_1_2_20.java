3817173da59ea529cba76c9ffcc4b6fcf1dc43a2
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-01 19:30:50 +0700

    Solved task 1.2.20

---
+++ src/Task_1_2_20.java
@@ -0,0 +1,9 @@
+public class Task_1_2_20 {
+    public static void main(String[] args) {
+        System.out.println(generate() + generate());
+    }
+
+    private static int generate() {
+        return (int) (1 + Math.random() * 6);
+    }
+}
