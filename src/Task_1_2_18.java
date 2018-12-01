cdc6eea89076558eb766b91370232688a29bf97a
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-01 14:09:42 +0700

    Solved task 1.2.18

---
+++ src/Task_1_2_18.java
@@ -0,0 +1,12 @@
+public class Task_1_2_18 {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            return;
+        }
+
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+
+        System.out.println(Math.sqrt(x * x + y * y));
+    }
+}
