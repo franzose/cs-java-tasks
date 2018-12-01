7def8598cd5795bde5fb2424f4b9f49d9c604ab1
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-01 14:09:13 +0700

    Solved task 1.2.14

---
+++ src/Task_1_2_14.java
@@ -0,0 +1,14 @@
+public class Task_1_2_14 {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            return;
+        }
+
+        int first = Integer.parseInt(args[0]);
+        int second = Integer.parseInt(args[1]);
+
+        if (first % second == 0 || second % first == 0) {
+            System.out.println("true");
+        }
+    }
+}
