f1c315baa04b52d13f7dd5a95ae14b6b3ad4403b
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-01 20:26:06 +0700

    Solved task 1.2.24

---
+++ src/Task_1_2_24.java
@@ -0,0 +1,19 @@
+public class Task_1_2_24 {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            return;
+        }
+
+        int t = Integer.parseInt(args[0]);
+        double P = Double.parseDouble(args[1]);
+        double r = Double.parseDouble(args[2]);
+
+        double result = calc(t, P, r);
+
+        System.out.println(result);
+    }
+
+    private static double calc(int t, double P, double r) {
+        return P * Math.pow(Math.E, r * t);
+    }
+}
