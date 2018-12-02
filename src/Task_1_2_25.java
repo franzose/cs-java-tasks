04a7f9d84c3f1394906a226d208d0ba4575471c6
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-02 18:38:00 +0700

    Solved task 1.2.25

---
+++ src/Task_1_2_25.java
@@ -0,0 +1,18 @@
+public class Task_1_2_25 {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            return;
+        }
+
+        double temperature = Double.parseDouble(args[0]);
+        double velocity = Double.parseDouble(args[1]);
+
+        double w = w(temperature, velocity);
+
+        System.out.println(w);
+    }
+
+    private static double w(double temperature, double velocity) {
+        return 35.74 + 0.6125 * temperature + (0.4275 * temperature - 35.75) * Math.pow(velocity, .16);
+    }
+}
