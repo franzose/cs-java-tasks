4735aaafb4924938137325619346488c02ada2e0
Jan Iwanow <iwanow.jan@gmail.com>
2018-12-18 22:26:32 +1100

    Solved task 1.3.29

---
+++ src/Task_1_3_29.java
@@ -0,0 +1,20 @@
+// Checkerboard
+public class Task_1_3_29 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+
+        int n = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String symbol = i % 2 == 0 ? " *" : "* ";
+
+                System.out.print(symbol);
+            }
+
+            System.out.println();
+        }
+    }
+}
