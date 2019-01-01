e003ae7b9371feb31dcd83ae392bbe42adb53a82
Jan Iwanow <iwanow.jan@gmail.com>
2019-01-01 21:00:09 +1100

    Solved task 1.4.11

---
+++ src/section_1_4/Task_1_4_11.java
@@ -0,0 +1,8 @@
+package section_1_4;
+
+// HowMany
+public class Task_1_4_11 {
+    public static void main(String[] args) {
+        System.out.println(String.format("You put %d arguments", args.length));
+    }
+}
